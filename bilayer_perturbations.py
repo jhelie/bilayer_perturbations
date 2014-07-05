965ca9661bb89d7208913821a52deefbcfc93af9
Jean Helie <orie2528@plasma.(none)>
2014-07-05 20:16:38 +0100

    v0.1.9

--- bilayer_perturbations.py
+++ bilayer_perturbations.py
@@ -12,7 +12,7 @@ import os.path
 #=========================================================================================
 # create parser
 #=========================================================================================
-version_nb="0.1.8-dev3"
+version_nb="0.1.9"
 parser = argparse.ArgumentParser(prog='bilayer_perturbations', usage='', add_help=False, formatter_class=argparse.RawDescriptionHelpFormatter, description=\
 '''
 ****************************************************
